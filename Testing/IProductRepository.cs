feaa7c658150e116b4020ff7f736c61695ac94e5
Zack Moyal <zack.moyal@gmail.com>
2024-04-29 06:54:51 -0700

    got Products to successfully display data from bestbuy on the webpage

---
+++ Testing/IProductRepository.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using Testing.Models;
+
+namespace Testing
+{
+    public interface IProductRepository
+    {
+        public IEnumerable<Product> GetAllProducts();
+    }
+}
---
+++ Testing/ProductRepository.cs
@@ -0,0 +1,22 @@
+﻿using Dapper;
+using System.Collections.Generic;
+using System.Data;
+using Testing.Models;
+
+namespace Testing
+{
+    public class ProductRepository : IProductRepository
+    {
+        private readonly IDbConnection _conn;
+
+        public ProductRepository(IDbConnection conn)
+        {
+            _conn = conn;
+        }
+
+        public IEnumerable<Product> GetAllProducts()
+        {
+            return _conn.Query<Product>("Select * From products");
+        }
+    }
+}
